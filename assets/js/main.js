9187a262eee12634ace03cc175fa391c07c0d711
Shahindev85 <86867455+Shahindev85@users.noreply.github.com>
2021-10-16 15:31:59 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.banner-area').slick({
+        prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-long-arrow-alt-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fas fa-long-arrow-alt-right"></i></button>',
+        autoplay: true
+    });
+  })
